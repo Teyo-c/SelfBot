781ce7287b3facae2d1bccdba8bbeca52f0f4773
Théo <>
2021-05-17 07:22:30 +0000

    Grosse optimisation du code avec utilisation de pytz

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import discord
 import os
-import time
 import asyncio
+import discord
+from datetime import datetime
 from keep_alive import keep_alive
+import pytz
+
+EU = pytz.timezone('Europe/Paris')
 
 client = discord.Client()
 
@@ -11,20 +14,13 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(name="Règle son horloge"))
 
-
 async def change_status():
     await client.wait_until_ready()
     while not client.is_closed():
-        t = time.gmtime()
-        Heure = time.strftime("%H", t)
-        Min = time.strftime("%M", t)
-        horaire = "⏲"
-        horaire+=str(int(Heure)+1)
-        horaire+=":"
-        horaire+=Min
-        horaire += ":"
-        horaire += time.strftime("%S", t)
-        horaire += "⏲"
+        t = datetime.now(EU)
+        horaire = " ⏲"
+        horaire += t.strftime("%H:%M:%S")
+        horaire += " ⏲"
         await client.change_presence(activity=discord.Game(name=horaire))
         await asyncio.sleep(5)
 
